18c8e3ada9f7fe40a6072dd5847e7609cdbbb888
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-29 10:36:32 +0100

    request for user location and get weather

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-/* eslint-disable no-console */
 import './app.css';
+import { getWeatherByCityName, getWeatherByCords } from './utils/fetch';
+
+const successfulLookup = position => {
+  const { latitude, longitude } = position.coords;
+  const { name, weather } = getWeatherByCords(latitude, longitude);
+  console.log(name, weather);
+};
+
+if (window.navigator.geolocation) {
+  window.navigator.geolocation.getCurrentPosition(successfulLookup, () => getWeatherByCityName('Lagos'));
+} else {
+  
+}
