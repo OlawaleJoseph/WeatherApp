b41a89629eddb419d3862e1e7cedd389b487e7a9
OlawaleJoseph <adedekojoseph@gmail.com>
2020-09-29 10:59:36 +0100

    request for lagos weather if user refuses location request

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './app.css';
 import { getWeatherByCityName, getWeatherByCords } from './utils/fetch';
 
-const successfulLookup = position => {
+const successfulLookup = async (position) => {
   const { latitude, longitude } = position.coords;
-  const { name, weather } = getWeatherByCords(latitude, longitude);
-  console.log(name, weather);
+  const { name, weather } = await getWeatherByCords(latitude, longitude);
+  console.log('Accept', name, weather);
+};
+
+const unsuccessfulLookup = async () => {
+  const { name, weather } = await getWeatherByCityName('Lagos');
+  console.log('Accept', name, weather);
 };
 
 if (window.navigator.geolocation) {
-  window.navigator.geolocation.getCurrentPosition(successfulLookup, () => getWeatherByCityName('Lagos'));
-} else {
-  
+  window.navigator.geolocation.getCurrentPosition(successfulLookup, unsuccessfulLookup);
 }
